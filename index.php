01895dd15f4640909fd5a4f2e33cd3d94bfa838c
Hasegawa kana <member002246@gmail.com>
2021-01-11 17:58:57 +0900

    phpテスト
    phpをテストで追加しました。

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php var_dump( $_POST ); ?>
+
+<?php
+  $total = 10;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>ご注文確認画面</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    
+    
+<!--    <?php echo $_POST['number']; ?>-->
+    
+    
+    
+    
+
+  </div><!--/wrapper-->
+</body>
+
+</html>
